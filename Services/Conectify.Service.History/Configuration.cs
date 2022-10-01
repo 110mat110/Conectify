158a9effcbc9c3e9f01acea16963c522425fe64e
Matej Kvetko <kvetko@develogi.cz>
2022-10-01 18:46:52 +0200

    HistroyConfig

--- Services/Conectify.Service.History/Configuration.cs
+++ Services/Conectify.Service.History/Configuration.cs
@@ -2,6 +2,10 @@
 {
     public class Configuration : Conectify.Services.Library.Configuration
     {
+        public Configuration(IConfiguration configuration) : base(configuration)
+        {
+        }
+
         public Guid SensorId { get; set; }
 
         public Guid ActuatorId { get; set; }
