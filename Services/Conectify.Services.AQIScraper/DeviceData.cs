7ea8372c2cb885ea03909929c89f0dea87fd9b55
110mat110 <33483661+110mat110@users.noreply.github.com>
2022-10-02 16:46:42 +0200

    AQI (#5)

---
+++ Services/Conectify.Services.AQIScraper/DeviceData.cs
@@ -0,0 +1,40 @@
+﻿using Conectify.Services.Library;
+using Conectify.Shared.Library.Models.Services;
+using Conectify.Shared.Library.Models;
+using Conectify.Shared.Services;
+
+namespace Conectify.Services.AQIScraper;
+
+public class DeviceData : IDeviceData
+{
+    private readonly Configuration configuration;
+
+    public DeviceData(Configuration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public ApiDevice Device => new()
+    {
+        Id = configuration.DeviceId,
+        IPAdress = WebFunctions.GetIPAdress(),
+        MacAdress = WebFunctions.GetMacAdress(),
+        Name = "Poruba weather"
+    };
+
+    public IEnumerable<ApiSensor> Sensors => new List<ApiSensor>()
+    {
+        new ApiSensor()
+        {
+            Id = configuration.SensorId,
+            Name = "AQI",
+            SourceDeviceId = configuration.DeviceId,
+        }
+    };
+
+    public IEnumerable<ApiPreference> Preferences => new List<ApiPreference>();
+
+    public IEnumerable<MetadataServiceConnector> MetadataConnectors => new List<MetadataServiceConnector>();
+
+    public IEnumerable<ApiActuator> Actuators => new List<ApiActuator>();
+}
