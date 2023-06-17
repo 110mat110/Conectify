f8d3e103776853023bd2bf7815d99187f7b7087b
ext_matej.kvetko@hptronic.cz <kvetko@develogi.cz>
2023-06-17 19:46:56 +0200

    Added support for Shelly plug

--- Services/Conectify.Services.ShellyConnector/Services/ValueScraper.cs
+++ Services/Conectify.Services.ShellyConnector/Services/ValueScraper.cs
@@ -31,7 +31,7 @@ public class ValueScraper : BackgroundService
                 string res = await client.GetStringAsync($"{configuration.ShellyIp}/rpc/Switch.GetStatus?id=0", stoppingToken);
 
                 var powerReading = JsonConvert.DeserializeAnonymousType(res, new { apower = float.NaN });
-                if (powerReading != null && powerReading.apower != float.NaN)
+                if (powerReading != null && !float.IsNaN(powerReading.apower))
                 {
                     await websocketClient.SendMessageAsync(new WebsocketBaseModel()
                     {
@@ -49,5 +49,38 @@ public class ValueScraper : BackgroundService
                 await Task.Delay(new TimeSpan(0, 1, 0), stoppingToken);
             }
         }
-    }
+
+		if (configuration.ShellyType.ToLower().Contains("plug"))
+        {
+			while (!stoppingToken.IsCancellationRequested)
+			{
+				using var client = new HttpClient();
+				client.DefaultRequestHeaders.Accept.Clear();
+				client.DefaultRequestHeaders.Accept.Add(
+					new MediaTypeWithQualityHeaderValue("application/json"));
+				client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+				string res = await client.GetStringAsync($"{configuration.ShellyIp}/meter/0", stoppingToken);
+
+				var powerReading = JsonConvert.DeserializeAnonymousType(res, new { power = float.NaN });
+				if (powerReading != null && !float.IsNaN(powerReading.power))
+				{
+					await websocketClient.SendMessageAsync(new WebsocketBaseModel()
+					{
+						Id = Guid.NewGuid(),
+						Name = "Power",
+						NumericValue = powerReading.power,
+						SourceId = configuration.PowerSensorId,
+						TimeCreated = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+						Type = "Value",
+						Unit = "W",
+						StringValue = string.Empty
+					}, stoppingToken);
+				}
+
+				await Task.Delay(new TimeSpan(0, 1, 0), stoppingToken);
+			}
+		}
+
+	}
 }
