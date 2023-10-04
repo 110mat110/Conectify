30488eef21a61878d297aa9e152f8fb8760047fa
ext_matej.kvetko@hptronic.cz <kvetko@develogi.cz>
2023-10-04 09:10:02 +0200

    fixed temporary url in config

--- Conectify.UI/src/environments/environment.ts
+++ Conectify.UI/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   serverUrl: "http://server.home:5000",
-  automatizationURL: "http://localhost:5021",
+  automatizationURL: "http://server.home:5021",
   historyUrl: "http://server.home:5020",
   websocketUrl: "ws://server.home:5000/api/websocket/"
 };
