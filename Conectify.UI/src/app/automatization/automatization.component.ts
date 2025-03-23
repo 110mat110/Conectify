4b5318419db840097d4d59df698b3cac09f2d156
ext_matej.kvetko@hptronic.cz <kvetko@develogi.cz>
2025-03-23 13:25:07 +0100

    automatization visual

--- Conectify.UI/src/app/automatization/automatization.component.ts
+++ Conectify.UI/src/app/automatization/automatization.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MessagesService } from '../messages.service';
 import { AutomatizationBase } from 'src/models/automatizationComponent';
 import { CdkDragEnd } from '@angular/cdk/drag-drop';
@@ -8,33 +8,13 @@ import { BehaviourMenuItem } from 'src/models/Automatization/BehaviourMenuItem';
 import { EditRule } from 'src/models/Automatization/EditRule';
 import { RuleConnection } from 'src/models/Automatization/RuleConnection';
 import { MatButton } from '@angular/material/button';
+import LeaderLine from 'leader-line-new';
 
 
 
 interface Rule {
   value: string;
   viewValue: string;
-}
-
-export class Polyline {
-  stroke: string;
-  x1: number;
-  y1: number;
-  x2: number;
-  y2: number;
-  constructor(
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-    stroke?: string,
-  ) {
-    this.x1 = x1;
-    this.x2 = x2;
-    this.y1 = y1;
-    this.y2 = y2;
-    this.stroke = stroke || "yellow";
-  }
 }
 
 export class ButtonReferences { elementRef: MatButton; id: string; constructor(elementRef: MatButton, id: string) { this.elementRef = elementRef, this.id = id } };
@@ -45,24 +25,26 @@ export class ButtonReferences { elementRef: MatButton; id: string; constructor(e
   styleUrls: ['./automatization.component.css']
 })
 
-export class AutomatizationComponent implements OnInit {
+export class AutomatizationComponent implements OnInit, OnDestroy, AfterViewInit {
 
   selectedRuleId: string = "";
   constructor(public messenger: MessagesService, public ruleService: RuleProviderService, public be: BefetchAutomatizationService) { }
+  ngAfterViewInit(): void {
+    this.ReDrawConnections();
+  }
   lineToFill: number = 0;
   lineHeight: number = 150;
   connections: RuleConnection[] = [];
-  polylines: Polyline[] = [];
   source?: string;
   destination?: string;
   references: ButtonReferences[] = [];
   supportedRules: BehaviourMenuItem[] = [];
+  lines: LeaderLine[] = [];
   @ViewChild('rules') rulesCanvas?: ElementRef<SVGElement>;
 
   ngOnInit(): void {
     this.be.GetAllBehaviours().subscribe(x => {
       this.supportedRules = x;
-      this.DrawConnections();
     }, (err) => {
       console.error(JSON.stringify(err));
     }
@@ -85,8 +67,6 @@ export class AutomatizationComponent implements OnInit {
   handleButtonGenerated(event: { elementRef: MatButton; id: string }): void {
     console.log('Generated Button ID:', event.id);
     this.references.push(new ButtonReferences(event.elementRef, event.id));
-
-    this.DrawConnections();
   }
 
   dragEnd(event: CdkDragEnd, rule: AutomatizationBase) {
@@ -120,11 +100,18 @@ export class AutomatizationComponent implements OnInit {
   }
 
   MoveComponent() {
-    this.DrawConnections();
+    this.ReDrawConnections();
+  }
+
+  ReDrawConnections(){
+    this.lines.forEach(line => {
+      line.position();
+    });
   }
 
   DrawConnections() {
-    this.polylines = [];
+    this.lines.forEach(line => line.remove()); // Remove all old lines
+    this.lines = [];
     this.connections.forEach(connecion => {
       console.log("Drawing line")
 
@@ -132,25 +119,43 @@ export class AutomatizationComponent implements OnInit {
       let destinationElement = this.references.find(x => x.id == connecion.destinationId);
 
       if (sourceElement?.elementRef._elementRef.nativeElement && destinationElement?.elementRef._elementRef.nativeElement) {
-        this.DrawLine(sourceElement?.elementRef._elementRef.nativeElement, destinationElement?.elementRef._elementRef.nativeElement);
+        this.AddLine(sourceElement?.elementRef._elementRef.nativeElement, destinationElement?.elementRef._elementRef.nativeElement);
       }
-
-
-    })
+    });
+    this.ReDrawConnections();
   }
 
-  DrawLine(destination: HTMLElement, source: HTMLElement) {
-    const { x: dx, y: dy, height: dh, width: dw } = destination.getBoundingClientRect();
-    const { x: sx, y: sy, height: sh, width: sw } = source.getBoundingClientRect();
-
-    let verticalOffset = this.rulesCanvas?.nativeElement.getBoundingClientRect().top ?? 0;
-    let horizontalOffset = this.rulesCanvas?.nativeElement.getBoundingClientRect().left ?? 0;
-    this.polylines.push(new Polyline(dx + (dw / 2) - horizontalOffset, dy + (dh / 2) - verticalOffset, sx + (sw / 2) - horizontalOffset, sy + (sh / 2) - verticalOffset))
-  }
+  AddLine(source: HTMLElement, destination: HTMLElement) {
+    var line = new LeaderLine(
+      source,
+      destination,
+      {
+        color: '#cccccc',
+        size: 3,
+        startSocket: 'right',
+        endSocket: 'left',
+        path: 'fluid', // Options: 'straight', 'arc', 'fluid', 'magnet', 'grid'
+        startPlug: 'behind',
+        endPlug: 'arrow3',
+        startPlugSize: 1,
+        endPlugSize: 1.5,
+        startPlugColor: '#d1b906',
+        endPlugColor: '#bf5102',
+        gradient: true,
+        dash: {animation: true} // Animated dashed line
+      }
+    );
+    this.lines.push(line);
+  };
 
   AddCustomInput(inputName: string) {
     this.be.createActuator({ actuatorName: inputName });
 
     this.ruleService.LoadAllRules();
+  }
+
+  ngOnDestroy() {
+    // Remove the leader line when the component is destroyed
+    this.lines.forEach(line => line.remove()); // Remove all old lines
   }
 }
