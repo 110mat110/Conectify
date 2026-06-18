c843daaf5720c6fc87324fe4fa8a961c23ba72fb
Matej Kvetko <kvetko@develogi.cz>
2026-06-18 19:35:06 +0200

    settings fix

--- Conectify.UI/src/environments/environment.prod.ts
+++ Conectify.UI/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   automatizationURL: "http://server.home:5021",
   historyUrl: "http://server.home:5020",
   dashboardUrl: "http://server.home:5022",
-  uiServiceUrl: "http://server.home:5023",
+  uiServiceUrl: "http://server.home:5030",
   websocketUrl: "ws://server.home:5000/api/websocket/",
   serverHome: "server.home",
   localApps:""
